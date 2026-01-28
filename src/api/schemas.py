19312ae918d2ccea8455e7ed8e5e445b4b626b20
henriqueluza <henriqueluza@gmail.com>
2026-01-28 16:34:18 -0300

    Adiciona descrição nos Fields do ClientInput

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel, Field
 
 class ClientInput(BaseModel):
-    renda_mensal: float = Field(ge=0)
-    tem_atraso_grave: bool
-    total_atrasos: int = Field(ge=0)
-    idade: int = Field(ge=18) # maior que 18
-    limite: float = Field(gt=0)
-    uso_limite: float = Field(ge=0)
-    divida: float = Field(ge=0)# para calcular a razao_divida_renda
-    num_dependentes: int = Field(ge=0) # positivo
-    linhas_credito_abertas: int = Field(ge=0)
-    emprestimos_imobiliarios: int = Field(ge=0)
+    renda_mensal: float = Field(ge=0, description="Quanto o cliente ganha por mês")
+    tem_atraso_grave: bool = Field(description="Teve algum atraso grave (Maior que 90 dias)?")
+    total_atrasos: int = Field(ge=0, description="Quantas vezes atrasou o pagamento do cartão de crédito?")
+    idade: int = Field(ge=18, description='Idade') # maior que 18
+    limite: float = Field(gt=0, description='Qual o limite do cartão?')
+    uso_limite: float = Field(ge=0, description="Quanto usou do limite?")
+    divida: float = Field(ge=0, description="Quanto tem de dívida?") # para calcular a razao_divida_renda
+    num_dependentes: int = Field(ge=0, description="Quantos dependentes financeiros?") # positivo
+    linhas_credito_abertas: int = Field(ge=0, description="Quantas linhas de crédito abertas?")
+    emprestimos_imobiliarios: int = Field(ge=0, description="Quantos empréstimos imobiliários o cliente possui?")
 
 class PredictionResponse(BaseModel):
     client_id: int
