63c32650ac594652244be5a6ed19260dabdda665
henriqueluza <henriqueluza@gmail.com>
2026-01-28 17:02:08 -0300

    Cria função transform_input

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,4 +1,6 @@
 import joblib
+import numpy as np
+from src.api.schemas import ClientInput
 
 MODEL_PATH = 'src/models/logistic_regression_final.joblib'
 SCALER_PATH = 'src/models/scaler.joblib'
@@ -16,3 +18,28 @@ def get_scaler():
 
 def get_features_names():
     return feature_names
+
+def transform_input(client: ClientInput) -> list:
+    log_renda_mensal = np.log1p(client.renda_mensal)
+    uso_credito_rotativo = client.uso_limite / client.limite
+
+    if client.renda_mensal > 0:
+        razao_divida_renda = client.divida/client.renda_mensal
+    else: razao_divida_renda = 0
+
+    razao_extrema = 1 if razao_divida_renda > 1 else 0
+
+    features = [
+        log_renda_mensal,
+        int(client.tem_atraso_grave),
+        client.total_atrasos,
+        client.idade,
+        uso_credito_rotativo,
+        razao_divida_renda,
+        razao_extrema,
+        client.num_dependentes,
+        client.linhas_credito_abertas,
+        client.emprestimos_imobiliarios
+    ]
+
+    return features
