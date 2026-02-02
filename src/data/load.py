32c2175d19d0d1eaac3468043a3a2d9bd71ef8af
henriqueluza <henriqueluza@gmail.com>
2026-02-01 21:42:39 -0300

    Ajuste na estrutura e planejamento do projeto

--- src/data/load.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from src.utils.database import get_engine
-
-def save_to_table(df: pd.Dataframe, table_name: str, if_exists: str = 'append'):
-    engine = get_engine()
-    df.to_sql(table_name, engine, if_exists=if_exists, index=False)
