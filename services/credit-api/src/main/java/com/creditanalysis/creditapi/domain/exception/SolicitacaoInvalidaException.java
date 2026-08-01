c095cb1316cb9e5b2c01e22e53809faa18552e6f
Henrique Luza <henriqueluza@gmail.com>
2026-08-01 20:49:00 -0300

    feat(spring): adicionar entidades e value objects do domínio Análise de Crédito

---
+++ services/credit-api/src/main/java/com/creditanalysis/creditapi/domain/exception/SolicitacaoInvalidaException.java
@@ -0,0 +1,8 @@
+package com.creditanalysis.creditapi.domain.exception;
+
+public class SolicitacaoInvalidaException extends RuntimeException {
+
+    public SolicitacaoInvalidaException(String message) {
+        super(message);
+    }
+}
