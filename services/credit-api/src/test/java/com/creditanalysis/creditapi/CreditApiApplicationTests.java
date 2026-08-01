82dd4ae24c98864ca2120490b43d1f78fc468e69
Henrique Luza <henriqueluza@gmail.com>
2026-08-01 20:24:29 -0300

    chore(spring): inicializar projeto Spring Boot com Flyway e datasource Postgres

---
+++ services/credit-api/src/test/java/com/creditanalysis/creditapi/CreditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditanalysis.creditapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
